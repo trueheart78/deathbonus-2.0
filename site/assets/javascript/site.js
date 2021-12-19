2bceec0534bf5eceaa2ddc3f45e08743b8c9afa2
Josh Mills <josh@trueheart78.com>
2021-12-18 20:38:07 -0500

    Cleanup Js and make it newer and exciting, circa 2015.

--- site/assets/javascript/site.js
+++ site/assets/javascript/site.js
@@ -1,21 +1,16 @@
 function relocate(chat, subscribe) {
-  var channelName = document.getElementById("channelName").value.toLowerCase()
+  const channelName = document.getElementById("channelName").value.toLowerCase();
   if (channelName == "") {
-    channelName = "dexbonus"
+    channelName = "dexbonus";
   }
   if (chat) {
-    window.location = `https://www.twitch.tv/popout/${channelName}/chat?no-mobile-redirect=true`
+    window.location = `https://www.twitch.tv/popout/${channelName}/chat?no-mobile-redirect=true`;
   } else if(subscribe) {
-    window.location = `https://www.twitch.tv/subs/${channelName}`
+    window.location = `https://www.twitch.tv/subs/${channelName}`;
   } else {
-    window.location = `http://go.twitch.tv/${channelName}`
+    window.location = `http://go.twitch.tv/${channelName}`;
   }
 }
 
 //creates a listener for when you press a key
-window.onkeyup = keyup;
-function keyup(e) {
-  if (e.keyCode == 13) {
-    relocate()
-  }
-}
+window.onkeyup = e => if (e.keyCode == 13) { relocate() }
