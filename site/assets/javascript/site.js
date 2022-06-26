7199700d0c76ca05cac5c5fade41819d9ac1b44c
Josh Mills <josh@trueheart78.com>
2022-06-26 13:24:22 -0400

    Simplified JS

--- site/assets/javascript/site.js
+++ site/assets/javascript/site.js
@@ -1,15 +1,30 @@
-function relocate(chat, subscribe) {
+function channel() {
   let channelName = document.getElementById("channelName").value.toLowerCase();
   if (channelName == "") {
     channelName = "dexbonus";
   }
-  if (chat) {
-    window.location = `https://www.twitch.tv/popout/${channelName}/chat?no-mobile-redirect=true`;
-  } else if(subscribe) {
-    window.location = `https://www.twitch.tv/subs/${channelName}`;
-  } else {
-    window.location = `http://go.twitch.tv/${channelName}`;
-  }
+  
+  return channelName;
+}
+
+function about() {
+  window.location = `https://www.twitch.tv/${channel()}/about`;
+}
+
+function chat() {
+  window.location = `https://www.twitch.tv/popout/${channel()}/chat?no-mobile-redirect=true`;
+}
+
+function subscribe() {
+  window.location = `https://www.twitch.tv/subs/${channel()}`;
+}
+
+function videos() {
+  window.location = `https://www.twitch.tv/${channel()}/videos`;
+}
+
+function watch() {
+  window.location = `http://go.twitch.tv/${channel()}`;
 }
 
 //creates a listener for when you press a key
